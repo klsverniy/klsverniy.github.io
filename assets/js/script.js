6b4fbd477bf8c513711ffefe07e302d831b24f78
deathkv20 <deathkv2.0@gmail.com>
2019-08-05 18:55:36 +0300

    eor

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,8 +4,12 @@ var slider = document.getElementById("lopra")
 
 slide.addEventListener("click",function hide (){
     soo.classList.add("hidden")
+    slide.classList.add("off")
+    slider.classList.remove("off")
 })
 
 slider.addEventListener("click",function show(){
     soo.classList.remove("hidden")
+    slide.classList.remove("off")
+    slider.classList.add("off")
 })
